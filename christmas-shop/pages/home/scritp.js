b33dee7f25b4b4b5caa76c50c6a92f6131a13f5f
Nadzeya Kaliada <hopenadya88@gmail.com>
2024-12-01 14:02:29 +0100

    feat: remove scroll when burger menu is opened and add smooth hiding of the menu and transforming of the cross on home page

--- christmas-shop/pages/home/scritp.js
+++ christmas-shop/pages/home/scritp.js
@@ -1,6 +1,21 @@
 let burgerButton = document.querySelector('.burger-button');
 let navMenu = document.querySelector('.nav-menu');
-burgerButton.addEventListener('click',()=>{
+let menuItemLink = document.querySelectorAll('.menu-item>.link');
+
+// Burger button pressing
+burgerButton.addEventListener('click',toggleMenu);
+// Menu-item pressing
+for (let item of menuItemLink){
+    item.addEventListener('click', toggleMenu);
+} 
+
+function toggleMenu(){
     burgerButton.classList.toggle('active');
     navMenu.classList.toggle('open');
-})
+    if(navMenu.classList.contains('open')){
+        document.body.style.overflow = 'hidden';
+    } else{
+        document.body.style.overflow = '';
+    }
+}
+
