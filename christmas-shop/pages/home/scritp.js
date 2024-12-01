e013296ac492b80ecfbfce33cafa1a973f2e22b4
Nadzeya Kaliada <hopenadya88@gmail.com>
2024-12-01 14:45:00 +0100

    feat: add non-smooth hiding of the menu and transforming of the cross on home page

--- christmas-shop/pages/home/scritp.js
+++ christmas-shop/pages/home/scritp.js
@@ -1,15 +1,21 @@
 let burgerButton = document.querySelector('.burger-button');
 let navMenu = document.querySelector('.nav-menu');
 let menuItemLink = document.querySelectorAll('.menu-item>.link');
-
+let burgerLines = document.querySelectorAll('.burger-line');
 // Burger button pressing
-burgerButton.addEventListener('click',toggleMenu);
+burgerButton.addEventListener('click',toggleMenuSamePage);
 // Menu-item pressing
-for (let item of menuItemLink){
-    item.addEventListener('click', toggleMenu);
+// Menu-item on the other page
+menuItemLink[0].addEventListener('click', toggleMenuOtherPage);
+
+// Menu-item on the same page
+for (let i=1; i<menuItemLink.length-1; ++i){
+
+    menuItemLink[i].addEventListener('click', toggleMenuSamePage);
+    // First menu-item which goes to another page
 } 
 
-function toggleMenu(){
+function toggleMenuSamePage(){
     burgerButton.classList.toggle('active');
     navMenu.classList.toggle('open');
     if(navMenu.classList.contains('open')){
@@ -17,5 +23,22 @@ function toggleMenu(){
     } else{
         document.body.style.overflow = '';
     }
+}
+
+function toggleMenuOtherPage(){
+    navMenu.style.transition = 'left 0s, opacity 0s';
+    for(let line of burgerLines){
+        line.style.transition = 'transform 0s';
+    }
+
+    burgerButton.classList.toggle('active');
+    navMenu.classList.toggle('open');
+    if(navMenu.classList.contains('open')){
+        document.body.style.overflow = 'hidden';
+    } else{
+        document.body.style.overflow = '';
+    }
+    navMenu.style.transition = 'left 0.9s ease-out, opacity 0.4s ease-out;';
+    line.style.transition = 'transform 0.5s ease-out';
 }
 
