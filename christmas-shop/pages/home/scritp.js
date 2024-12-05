5dbf7daf91e33a63c3f09060517a6e775637e373
Nadzeya Kaliada <hopenadya88@gmail.com>
2024-12-05 17:50:51 +0100

    fix: remove closing modal when clicked on modal card space

--- christmas-shop/pages/home/scritp.js
+++ christmas-shop/pages/home/scritp.js
@@ -223,13 +223,18 @@ function generateCards(n){ // n is how many cards should be generated
 
 //********************************************************* Modal ************************************************************
 document.querySelector('.cross-button').addEventListener('click', closeModal);
+document.querySelector('.modal-content').addEventListener('click',(event)=>{
+    event.stopPropagation();
+})
 document.querySelector('.modal').addEventListener('click', closeModal);
 
 function openModal(cardIndex){
-    const layout = document.querySelector('.layout-container');
-    layout.style.paddingRight = '17px';
-    layout.style.maxWidth = '91.0625rem';
-
+    /*if(document.documentElement.clientWidth > 768){
+        const layout = document.querySelector('.layout-container');
+        layout.style.paddingRight = '17px';
+        layout.style.maxWidth = '91.0625rem';
+    }*/
+    
     const modal = document.querySelector('.modal');
     modal.classList.add('open');
 
@@ -303,9 +308,12 @@ function closeModal(){
     const modal = document.querySelector('.modal');
     modal.classList.remove('open');
     document.body.style.overflow = '';
-    const layout = document.querySelector('.layout-container');
-    layout.style.paddingRight = '';
-    layout.style.maxWidth = '90rem';
+    /*
+    if(document.documentElement.clientWidth > 768){
+        const layout = document.querySelector('.layout-container');
+        layout.style.paddingRight = '';
+        layout.style.maxWidth = '90rem';
+    }*/
 }
 
 
